9e75a6fe05ae1ca6ecd37d2eaae225b8c1914662
Mantvydas Donauskas <mantvydas.donauskas@ktu.edu>
2018-03-10 20:05:08 +0200

    Changed table name to lower case.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ORM\Table(name="User")
+ * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User implements UserInterface, \Serializable
