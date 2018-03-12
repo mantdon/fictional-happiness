4116a632545f40198100c8fa08abbeb34bb3da83
Martynas Treinys <martynas7928@gmail.com>
2018-03-12 19:08:02 +0200

    basic profile page created

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,13 +6,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HomeController extends Controller
+/**
+ * @Route("/user")
+ */
+class UserController extends Controller
 {
+
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="user_profile")
      */
-    public function home()
+    public function showProfile()
     {
-        return $this->render('base.html.twig');
+
+        return $this->render(
+        'profile.html.twig'
+    );
     }
 }
