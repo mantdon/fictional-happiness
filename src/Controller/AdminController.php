fc3533466923e20c612d3c878815cb534ab5ff13
Martynas Treinys <martynas7928@gmail.com>
2018-03-23 20:09:23 +0200

    Added controller for admin panel.

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @Route("/admin")
+ */
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="admin.homepage")
+     */
+    public function home()
+    {
+        return $this->render('Admin/base.html.twig');
+    }
+}
