406ccb45aae32d8179438c61955a5b65c36f54bb
Mantvydas Donauskas <mantvydas.donauskas@ktu.edu>
2018-03-24 19:00:00 +0200

    Added a controller for tabs in user profile.

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Vehicle;
+use App\Form\VehicleType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ProfileController extends Controller
+{
+	/**
+	 * @Route("/user/orders", name="user_orders")
+	 */
+	public function ordersTabActions(){
+		return $this->render('Profile/profile_orders.html.twig', array(
+			'user' => $this->getUser(),
+			//'orders' => $this->getUser()->getOrders()
+		));
+	}
+
+	/**
+	 * @Route("/user/vehicles", name="user_vehicles")
+	 */
+	public function vehiclesTabAction(){
+		return $this->render('Profile/profile_vehicles.html.twig', array(
+			'user' => $this->getUser(),
+			'vehicles' => $this->getUser()->getVehicles()
+		));
+	}
+
+	/**
+	 * @Route("/user/messages", name="user_messages")
+	 */
+	public function messagesTabAction(){
+		return $this->render('Profile/profile_messages.html.twig', array(
+			'user' => $this->getUser()
+		));
+	}
+
+	/**
+	 * @Route("/user/settings", name="user_settings")
+	 */
+	public function settingsTabAction(){
+		return $this->render('Profile/profile_settings.html.twig', array(
+			'user' => $this->getUser()
+		));
+	}
+	/**
+	 * @Route("/user/changepassword", name="user_changepassword")
+	 */
+	public function changepasswordTabAction(){
+		return $this->render('Profile/profile_changepassword.html.twig', array(
+			'user' => $this->getUser()
+		));
+	}
+}
