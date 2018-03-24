30c2dbfcda2b28f7b324d8960991858720d73541
Martynas Treinys <martynas7928@gmail.com>
2018-03-24 09:19:38 +0200

    Method to get paginated user list added.

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -1,9 +1,15 @@
 <?php
 namespace App\Repository;
 
+use App\Helpers\Pagination;
 use Doctrine\ORM\EntityRepository;
 
 class UserRepository extends EntityRepository
 {
-
+    public function getAll($currentPage = 1, $limit = 5)
+    {
+        $qb = $this->createQueryBuilder('s')->getQuery();
+        $paginator = Pagination::paginate($qb, $currentPage, $limit);
+        return $paginator;
+    }
 }
