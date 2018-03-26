22028b20ea4e1e5e01424241824d8ad47d2b3f10
Mantvydas Donauskas <mantvydas.donauskas@ktu.edu>
2018-03-26 22:50:36 +0300

    Added automatic log in after registration functionality.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Form\RegistrationType;
 use App\Entity\User;
 use App\Services\RegistrationHandler;
+use App\Services\UserManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,8 +23,16 @@ class RegistrationController extends Controller
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
 
-        if($registrationHandler->handleForm($form, $request))
-            return $this->redirectToRoute('homepage');
+        if($registrationHandler->handleForm($form, $request)) {
+	        $token = UserManager::createAuthenticationTokenFor($user);
+
+	        // Logs the user in
+	        // Remember me functionality is not present.
+	        $this->get('security.token_storage')->setToken($token);
+	        $this->get('session')->set('_security_main', serialize($token));
+
+	        return $this->redirectToRoute( 'homepage' );
+        }
 
         return $this->render(
             'registration.html.twig',
