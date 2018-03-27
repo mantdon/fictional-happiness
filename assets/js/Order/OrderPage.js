86f74ac602f32c5b46ff2fc3790e3858f9e5b119
Martynas Treinys <martynas7928@gmail.com>
2018-03-27 18:13:47 +0300

    Fixed error on dialog change.

--- assets/js/Order/OrderPage.js
+++ assets/js/Order/OrderPage.js
@@ -1,35 +1,47 @@
 import React from 'react';
 import {render} from 'react-dom';
 import VehicleSelection from './VehicleSelection';
-import ServiceSelection from './ServiceSelection';
+import ServicesSelection from './ServicesSelection';
 
 class OrderPage extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {step: 1};
+        this.state = {step: 1,
+        dialog: null};
         this.nextStep = this.nextStep.bind(this);
+    }
+
+    componentWillMount()
+    {
+        this.changeDialog(this.state.step);
+    }
+
+    changeDialog(step)
+    {
+        this.setState({dialog: this.getCurrentDialog(step)});
     }
 
     nextStep()
     {
         this.setState({
             step : this.state.step + 1
-        })
+        }, () => {this.changeDialog(this.state.step);});
+
     }
 
-    getCurrentDialog()
+    getCurrentDialog(step)
     {
-        switch (this.state.step) {
+        switch (step) {
             case 1:
                 return <VehicleSelection nextStep={this.nextStep}/>;
             case 2:
-                return <ServiceSelection nextStep={this.nextStep()}/>;
+                return <ServicesSelection nextStep={this.nextStep}/>;
         }
     }
 
     render() {
-        return <div className={'orderDialog'}>{this.getCurrentDialog()}</div>;
+        return <div className={'orderDialog'}>{this.state.dialog}</div>;
     }
 }
 
