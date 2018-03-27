e297c32fa888a021a2fd8858e12ea95cc2714358
Martynas Treinys <martynas7928@gmail.com>
2018-03-27 22:12:11 +0300

    Fixed class name.

--- assets/js/Order/ServicesSelection.js
+++ assets/js/Order/ServicesSelection.js
@@ -46,7 +46,7 @@ export default class ServicesSelection extends React.Component {
     render(){
         return <div>
                     <h1>Pasirinkite paslaugas</h1>
-            <div className={'vehicleOptionsContainer'}>{this.state.services}</div>
+            <div className={'optionsContainer'}>{this.state.services}</div>
                 </div>;
     }
 }
