00fb17293b97c260f80ef7b5d57068289cc70410
Martynas Treinys <martynas7928@gmail.com>
2018-03-27 22:05:14 +0300

    Formated service selection option.

--- assets/js/Order/ServiceOption.js
+++ assets/js/Order/ServiceOption.js
@@ -4,8 +4,8 @@ import {render} from 'react-dom';
 export default class ServiceOption extends React.Component {
     render(){
        return <div className={'Option'} onClick={this.props.nextStep}>
-                    <div> {this.props.service.name} </div>
-                    <div>{this.props.service.price}</div>
+                    <div className={'serviceName'}> {this.props.service.name} </div>
+                    <div className={'servicePrice'}>{this.props.service.price}</div>
                 </div>;
     }
 }
