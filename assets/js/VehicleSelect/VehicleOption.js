bbd753e1da312d2bfaf8e2356106d94eb1aad725
Martynas Treinys <martynas7928@gmail.com>
2018-03-27 17:32:14 +0300

    VehicleSelect component deleted.

--- assets/js/VehicleSelect/VehicleOption.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-export default class VehicleOption extends React.Component {
-    render() {
-        return <option> {this.props.vehicle.plateNumber} </option>;
-    }
-}
