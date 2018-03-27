aa1addc103993010b7ff8884afbe652b70b5db68
Martynas Treinys <martynas7928@gmail.com>
2018-03-27 21:53:06 +0300

    Css class vehicleOption renamed to Option to be more generic.

--- assets/js/Order/Option.js
+++ assets/js/Order/Option.js
@@ -4,6 +4,6 @@ import {render} from 'react-dom';
 export default class Option extends React.Component {
 
     render() {
-        return <div className={'vehicleOption'} onClick={this.props.nextStep}> {this.props.vehicle.plateNumber} </div>;
+        return <div className={'Option'} onClick={this.props.nextStep}> {this.props.vehicle.plateNumber} </div>;
     }
 }
--- assets/js/Order/ServiceOption.js
+++ assets/js/Order/ServiceOption.js
@@ -3,7 +3,7 @@ import {render} from 'react-dom';
 
 export default class ServiceOption extends React.Component {
     render(){
-       return <div className={'vehicleOption'} onClick={this.props.nextStep}>
+       return <div className={'Option'} onClick={this.props.nextStep}>
                     <div> {this.props.service.name} </div>
                     <div>{this.props.service.price}</div>
                 </div>;
