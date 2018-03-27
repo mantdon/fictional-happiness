4ef2d276dcd230ba85ee440637b385b05cc06975
Martynas Treinys <martynas7928@gmail.com>
2018-03-27 18:44:01 +0300

    ServiceSelection component created.

--- assets/js/Order/ServicesSelection.js
+++ assets/js/Order/ServicesSelection.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import VehicleOption from './Option';
 
 export default class OrderPage extends React.Component {
 
     render(){
-
+        return <div>
+                    <h1>Pasirinkite paslaugas</h1>
+                </div>;
     }
 }
