b8de20bd4dd6fd7deb95d8daaa2d2fd4cb8709f7
Martynas Treinys <martynas7928@gmail.com>
2018-03-27 22:50:36 +0300

    Wrong onClick function fixed.

--- assets/js/Order/ServiceOption.js
+++ assets/js/Order/ServiceOption.js
@@ -3,7 +3,7 @@ import {render} from 'react-dom';
 
 export default class ServiceOption extends React.Component {
     render(){
-       return <div className={'Option'} onClick={this.props.nextStep}>
+       return <div className={'Option'} onClick={this.props.onClick}>
                     <div className={'serviceName'}> {this.props.service.name} </div>
                     <div className={'servicePrice'}>{this.props.service.price}</div>
                 </div>;
