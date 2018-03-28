20d203e90889c5554d14ec417f27d367c1981eb0
daiarn <dainius1997@gmail.com>
2018-03-28 17:51:33 +0300

    Removed all styling

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -17,19 +17,17 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class, array('label' => 'El-paštas', 'attr' => array('class' => 'textField', 'style' => 'margin-bottom:15px')))
+            ->add('email', EmailType::class)
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options' => array('label' => 'Slaptažodis', 'attr' => array('class' => 'textField', 'style' => 'margin-bottom:15px')),
-                'second_options' => array('label' => 'Pakartoti slaptažodį', 'attr' => array('class' => 'textField', 'style' => 'margin-bottom:15px')),
                 'invalid_message' => 'Slaptažodžiai nesutampa'
             ))
-            ->add('first_name', TextType::class, array('label' => 'Vardas', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
-            ->add('last_name', TextType::class, array('label' => 'Pavardė', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
-            ->add('phone', TextType::class, array('label' => 'Telefonas', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
-            ->add('city', TextType::class, array('label' => 'Miestas', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
-            ->add('address', TextType::class, array('label' => 'Adresas', 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
-            ->add('submit', SubmitType::class, array('label' => 'Registruotis', 'attr' => array('class' => 'button', 'style' => 'margin-bottom:15px')));
+            ->add('first_name', TextType::class)
+            ->add('last_name', TextType::class)
+            ->add('phone', TextType::class)
+            ->add('city', TextType::class)
+            ->add('address', TextType::class)
+            ->add('submit', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
