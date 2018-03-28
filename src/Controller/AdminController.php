d26c2537c019d3ff075511328330d9a5c485b084
Mantvydas Donauskas <mantvydas.donauskas@ktu.edu>
2018-03-28 20:12:01 +0300

    Updated route names and template paths. Moved delete user method to user controller.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -15,28 +15,38 @@ class AdminController extends Controller
     private $pageParameterName = "page";
 
     /**
-     * @Route("/", name="admin.homepage")
+     * @Route("/", name="admin_home")
      */
     public function home()
     {
         return $this->render('Admin/Home/admin_home.html.twig');
     }
 
-    /**
-     * @Route("/users", name="admin.user_list")
-     */
-    public function showUserList(Request $request, PaginatedListFetcher $listFetcher)
-    {
-        $page = $request->query->getInt($this->pageParameterName, 1);
+	/**
+	 * @Route("/users", name="admin_users")
+	 */
+    public function usersAction(Request $request){
+	    $page = $this->loadPageValue($request, 1);
+	    return $this->redirectToRoute('admin_users_page', [$this->pageParameterName => $page]);
+    }
 
+	/**
+	 * @Route ("/users/{page}", name="admin_users_page", requirements={"page"="\d+"})
+	 */
+    public function usersPageAction(PaginatedListFetcher $listFetcher, $page)
+    {
         $list = $listFetcher->getPaginatedList('App:User', $page);
-        return $this->render('Admin/UserList/list.html.twig',
+	    // Load first page on invalid page entry.
+	    // 'totalCount' check is needed to avoid redirection loops on empty list.
+	    if($list['totalCount'] !== 0 && $page > $list['pageCount'] || $page < 1)
+		    return $this->redirectToRoute('admin_users_page', array($this->pageParameterName => 1));
+        return $this->render('Admin/Users/list.html.twig',
             array('users' => $list['items'],
                 'pageCount' => $list['pageCount'],
                 'userCount' => $list['totalCount'],
                 'currentPage' => $page,
                 'pageParameterName' => $this->pageParameterName,
-                'route' => 'admin.user_list'));
+                'route' => 'admin_users_page'));
     }
 
 	/**
@@ -50,7 +60,7 @@ class AdminController extends Controller
 	/**
 	 * @Route ("/services/{page}", name="admin_services_page", requirements={"page"="\d+"})
 	 */
-	public function servicePageAction(Request $request,  PaginatedListFetcher $listFetcher, $page){
+	public function servicePageAction(PaginatedListFetcher $listFetcher, $page){
 		$list = $listFetcher->getPaginatedList('App:Service', $page);
 		// Load first page on invalid page entry.
 		// 'totalCount' check is needed to avoid redirection loops on empty list.
@@ -105,18 +115,4 @@ class AdminController extends Controller
 
 		return $savedPage;
 	}
-
-    /**
-     * @Route("/users/delete/{id}", name="delete.user")
-     */
-    public function deleteUser($id)
-    {
-        $entityManager = $this->getDoctrine()->getEntityManager();
-        $user = $entityManager->getRepository('App:User')->find($id);
-        $entityManager->remove($user);
-        $entityManager->flush();
-        return $this->render('Admin/delete_user.html.twig', [
-        'id' => $id
-        ]);
-    }
 }
