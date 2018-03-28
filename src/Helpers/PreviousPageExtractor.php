f519fb4e316474cabbf00e4c11e03ecbcaac7d0a
Mantvydas Donauskas <mantvydas.donauskas@ktu.edu>
2018-03-28 20:10:21 +0300

    Generalized for use in multiple paginated lists.

--- src/Helpers/PreviousPageExtractor.php
+++ src/Helpers/PreviousPageExtractor.php
@@ -3,15 +3,14 @@
 namespace App\Helpers;
 
 class PreviousPageExtractor{
-	public static function getPreviousPage($previousPageURL, $pageParameterName){
+	public static function getPreviousPage(string $previousPageURL, string $routeBeforeNumber){
 		// A fail-safe, in case of invalid input.
 		$pageNumberMatch = 1;
 
-		// Matches "services/###"
-		preg_match("/services\/[0-9]+$/", $previousPageURL, $pageStringMatch);
-		dump($pageStringMatch);
+		// Matches "$routeBeforeNumber/###"
+		preg_match("/".$routeBeforeNumber."\/[0-9]+$/", $previousPageURL, $pageStringMatch);
 		if(count($pageStringMatch) == 1){
-			// Matches only ### from "services/###"
+			// Matches only ### from "$routeBeforeNumber/###"
 			preg_match( "/[0-9]+/", $pageStringMatch[0], $pageNumberMatch );
 		}
 
