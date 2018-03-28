f98807ecb1dc4252093f3c46bfeb69c798e45fbe
Martynas Treinys <martynas7928@gmail.com>
2018-03-28 15:21:18 +0300

    Selected services are not shown in available services list.

--- assets/js/Order/ServicesSelection.js
+++ assets/js/Order/ServicesSelection.js
@@ -8,6 +8,7 @@ export default class ServicesSelection extends React.Component {
         super(props);
         this.state = {
             services: [],
+            servicesList: [],
             selectedServices: [],
             selectedServicesList: [],
             searchValue: ''
@@ -29,7 +30,8 @@ export default class ServicesSelection extends React.Component {
             .then(
                 (result) => {
                     this.setState({
-                        services: this.formServiceList(result, this.selectService)
+                        services: result,
+                        servicesList: this.formAvailableServiceList(result, this.selectService)
                     });
                 },
                 (error) => {
@@ -46,15 +48,49 @@ export default class ServicesSelection extends React.Component {
         newList.push(service);
         this.setState({
             selectedServices: newList,
-            selectedServicesList: this.formServiceList(newList, null)}
+            selectedServicesList: this.formServiceList(newList, null)}, () => {
+                this.updateServiceList();
+            }
         );
+    }
+
+    updateServiceList()
+    {
+        this.setState({
+            servicesList: this.formAvailableServiceList(this.state.services, this.selectService)
+        });
+    }
+
+    formAvailableServiceList(services, onClick)
+    {
+        if(services.length > 0)
+            return services.map((service, i) => this.formAvailableService(service, i, onClick));
+    }
+
+    formAvailableService(service, i, onClick)
+    {
+        if(!this.isSelected(service))
+            return this.formService(service, i, onClick);
     }
 
     formServiceList(services, onClick)
     {
         if(services.length > 0)
-            return services.map((service, i) => <ServiceOption onClick={onClick} service={service}
-                                                                key={i}/>);
+            return services.map((service, i) => this.formService(service, i, onClick));
+    }
+
+    formService(service, i, onClick)
+    {
+        return <ServiceOption onClick={onClick} service={service}
+                              key={i}/>;
+    }
+
+    isSelected(service)
+    {
+        return this.state.selectedServices.some((srv) => {
+            if(srv.id === service.id)
+                return true;
+        });
     }
 
     handleSearchBox(event)
@@ -76,7 +112,7 @@ export default class ServicesSelection extends React.Component {
                                 onChange={this.handleSearchBox}
                             />
                             <div className={'scrollable-vertical searchableServices'}>
-                                {this.state.services}
+                                {this.state.servicesList}
                             </div>
                         </div>
                         <div className={'serviceOptionsContainer scrollable-vertical align-right'}>
