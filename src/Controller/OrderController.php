909db385c27c1b70a80da54cdb32ee4818e3ffc5
Martynas Treinys <martynas7928@gmail.com>
2018-04-07 11:28:25 +0300

    Test routes deleted.

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -11,32 +11,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class OrderController extends Controller
 {
     /**
-     * @Route("/order", name="order")
-     */
-    public function home()
-    {
-        //For testing purposes.
-        $array = array(
-            ['plateNumber' => 'asfa'],
-            ['plateNumber' => 'ahedfb'],
-        );
-        return $this->render('Order/base.html.twig',
-            array('vehicles' => $array));
-    }
-
-    /**
-     * @Route("/api")
-     */
-    public function api()
-    {
-        $array = array(
-            ['id' => 0, 'plateNumber' => 'asfa'],
-            ['id' => 1, 'plateNumber' => 'ahedfb'],
-        );
-        return new JsonResponse($array);
-    }
-
-    /**
      * @Route("order/submit")
      */
     public function submit(Request $request, OrderCreator $orderCreator)
