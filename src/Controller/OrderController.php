b86a912abefc5fa53c69162df269fc8190cfaf50
Martynas Treinys <martynas7928@gmail.com>
2018-04-09 12:10:29 +0300

    Vehicle creation modal pops up after opening registration for services page if user has no vehicles registered.

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Vehicle;
+use App\Form\VehicleType;
 use App\Services\OrderCreator;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,8 +15,26 @@ class OrderController extends Controller
     /**
      * @Route("/order", name="order")
      */
-    public function home()
+    public function home(Request $request)
     {
+        if($this->getUser()->getVehicles()->count() === 0) {
+            $vehicle = new Vehicle();
+            $form = $this->createForm( VehicleType::class, $vehicle);
+
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $vehicle->setUser($this->getUser());
+                $entityManager->persist($vehicle);
+                $entityManager->flush();
+            }
+            else
+                return $this->render('Order/order_vehicle_add', array(
+                    'form' => $form->createView()
+                ));
+        }
+
         return $this->render('Order/base.html.twig');
     }
 
