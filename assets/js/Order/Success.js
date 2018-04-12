d9c904ae57d6e323ee7a27c8f90b41368563b11e
Martynas Treinys <martynas7928@gmail.com>
2018-04-12 15:06:42 +0300

    Fixed wrong class name.

--- assets/js/Order/Success.js
+++ assets/js/Order/Success.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-export default class ServicesSelection extends React.Component {
+export default class Success extends React.Component {
     render(){
         return <h1>Užsakymas sėkmingas įvykdytas</h1>
     }
