18568024545823dcf0fb8b9e558ee40f816cc3ab
Mantvydas Donauskas <mantvydas.donauskas@ktu.edu>
2018-04-15 21:46:13 +0300

    Added view for messages in profile along with methods required for proper pagination. Added example routes to demonstrate how to send messages.

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,13 +4,19 @@ namespace App\Controller;
 
 use App\Entity\Vehicle;
 use App\Form\VehicleType;
+use App\Helpers\MessageManager;
+use App\Services\PaginatedListFetcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Helpers\Pagination;
+use \DateTime;
 
 class ProfileController extends Controller
 {
+	private $pageParameterName = "page";
+
 	/**
 	 * @Route("/user/orders", name="user_orders")
 	 */
@@ -34,10 +40,29 @@ class ProfileController extends Controller
 	/**
 	 * @Route("/user/messages", name="user_messages")
 	 */
-	public function messagesTabAction(){
-		return $this->render('Profile/Messages/profile_messages.html.twig', array(
-			'user' => $this->getUser()
-		));
+	public function messagesTabAction(Request $request){
+		$page = $this->loadPageValue($request);
+		return $this->redirectToRoute('user_message_page', [$this->pageParameterName => $page]);
+	}
+
+	/**
+	 * @Route("user/messages/page/{page}", name="user_message_page", requirements={"page"="\d+"})
+	 */
+	public function messagesPageAction(PaginatedListFetcher $listFetcher, $page){
+		$list = $listFetcher->getPaginatedMessagesList($this->getUser(),'App:MessageMetaData', $page, 4);
+
+		if($list['totalCount'] !== 0 && $page > $list['pageCount'] || $page < 1)
+			return $this->redirectToRoute('user_message_page', array($this->pageParameterName => 1));
+
+		return $this->render('Profile/Messages/profile_messages.html.twig',
+		                     array('user' => $this->getUser(),
+			                     'messages' => $list['items'],
+			                     'pageCount' => $list['pageCount'],
+			                     'messagesCount' => $list['totalCount'],
+			                     'currentPage' => $page,
+			                     'pageParameterName' => $this->pageParameterName,
+			                     'route' => 'user_message_page'
+		                     ));
 	}
 
 	/**
@@ -56,4 +81,68 @@ class ProfileController extends Controller
 			'user' => $this->getUser()
 		));
 	}
+
+	//<editor-fold desc="Message sending examples">
+	/**
+	 * @Route("/user/messages/send", name="send_message")
+	 */
+	public function messageToProfile(MessageManager $mm){
+		$messageTitle = "New message";
+		// Content is expected to be rendered from an html.
+		$messageContent = $this->renderView('Email/order_placed.html.twig');
+		// May be used when sending message to user's profile.
+		$customSender = 'Extremely witty name';
+		// User object should be retrieved from relevant entity.
+		// Sending to yourself at the moment.
+		$recipient = $this->getUser();
+
+		$message = $mm->fetchOrCreateMessage($messageTitle, $messageContent);
+		$mm->sendMessageToProfile($message, $recipient);
+
+		return $this->redirectToRoute("user_messages");
+	}
+
+	/**
+	 * @Route("/user/messages/sendemail", name="send_email_message")
+	 */
+	public function messageToEmail(MessageManager $mm)
+	{
+		$messageTitle = "New message";
+		// Content is expected to be rendered from an html.
+		$messageContent = $this->renderView('Email/order_placed.html.twig');
+		// User object should be retrieved from relevant entity.
+		// Sending to yourself at the moment.
+		$recipient = $this->getUser();
+
+		$message = $mm->fetchOrCreateMessage($messageTitle, $messageContent);
+		$mm->sendMessageToEmail($message, $recipient);
+
+		return $this->redirectToRoute("user_messages");
+	}
+	//</editor-fold>
+
+	//<editor-fold desc="TO-DO: CLEAN THIS UP!">
+	private function loadPageValue(Request $request, $defaultValue = 1){
+		$previous_page_key = 'previous_page';
+		$page = NULL;
+		if($this->previousPageRequired($previous_page_key))
+			$page = $this->loadAndClearPageFromSession($previous_page_key);
+		if($page === NULL)
+			return $defaultValue;
+		return $page;
+	}
+
+	private function previousPageRequired(string $previous_page_key){
+		if($this->get('session')->has($previous_page_key))
+			return true;
+		return false;
+	}
+
+	public function loadAndClearPageFromSession(string $page_key){
+		$savedPage = $this->get( 'session' )->get($page_key);
+		$this->get('session')->remove($page_key);
+
+		return $savedPage;
+	}
+	//</editor-fold>
 }
