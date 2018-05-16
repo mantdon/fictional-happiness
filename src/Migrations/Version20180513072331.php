6f129ceb2f42d822cc1d7ddf6f62e43fb62c9c31
„Mantas <mantasarlauskas1997@gmail.com>
2018-05-16 19:25:10 +0300

    Added ability for users to write a review

---
+++ src/Migrations/Version20180513072331.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180513072331 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE review DROP rates_sum, DROP times_rated, DROP calculated_rating, DROP parent_id, DROP created_at, DROP updated_at, DROP child');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE review ADD rates_sum INT DEFAULT 0 NOT NULL, ADD times_rated INT DEFAULT 0 NOT NULL, ADD calculated_rating INT DEFAULT 0 NOT NULL, ADD parent_id INT DEFAULT NULL, ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL, ADD child INT DEFAULT NULL');
+    }
+}
