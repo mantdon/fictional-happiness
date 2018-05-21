c868ac7ab120065a4256f1fecbb33848d5165aa7
Martynas Treinys <martynas7928@gmail.com>
2018-05-22 00:27:30 +0300

    Cleaned up migrations.

--- src/Migrations/Version20180521212640.php
+++ src/Migrations/Version20180521212640.php
@@ -8,15 +8,17 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180519150231 extends AbstractMigration
+class Version20180521212640 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE password_reset (id INT NOT NULL, user_id INT NOT NULL, expiration_date DATETIME NOT NULL, is_active TINYINT(1) NOT NULL, INDEX IDX_B1017252A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE password_reset (token VARCHAR(255) NOT NULL, user_id INT NOT NULL, expiration_date DATETIME NOT NULL, is_active TINYINT(1) NOT NULL, INDEX IDX_B1017252A76ED395 (user_id), PRIMARY KEY(token)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE password_reset ADD CONSTRAINT FK_B1017252A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE service CHANGE description description LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(50) NOT NULL');
     }
 
     public function down(Schema $schema)
@@ -25,5 +27,7 @@ class Version20180519150231 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE password_reset');
+        $this->addSql('ALTER TABLE service CHANGE description description VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE user CHANGE email email VARCHAR(25) NOT NULL COLLATE utf8mb4_unicode_ci');
     }
 }
